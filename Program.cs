65673265f15fd33eaf712737a5aa49a56d52688c
James Kulon <james.kulon@gmail.com>
2017-03-16 18:50:41 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericDictionary
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Create generic list of states
+            Dictionary<string, string> states = new Dictionary<string, string>();
+            states.Add("az", "Arizona");
+            states.Add("wi", "Wisconsin");
+            states.Add("il", "Illinois");
+
+            Console.WriteLine(states["az"]);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
